ed766a453232d30af64f23bc6fe0874b079d9f8f
Saurabh <saurabhkumar121201@gmail.com>
2024-07-27 11:12:56 +0530

    useState method

--- 02counter/src/App.js
+++ 02counter/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-let counter = 0
+// let counter = 0
 
-function AddCounter() {
-    counter = counter + 1
-}
 
-function RemoveCounter() {
-    counter -= 1;
-    console.log(counter)
-}
+
 
 function App() {
+
+    const [counter, setCounter] = useState(0)
+    const addValue = () => {
+        setCounter((counter) => (counter + 1))
+    }
+    const removeValue = () => {
+        setCounter(counter - 1)
+    }
     return ( <
         >
         <
         h1 > Counter { counter } < /h1>  <
-        button onClick = { AddCounter } > Add value < /button> <
-        button onClick = { RemoveCounter } > Remove value < /button>  < / >
+        button onClick = { addValue } > Add value < /button> <
+        button onClick = { removeValue } > Remove value < /button>  < / >
     );
 }
 
